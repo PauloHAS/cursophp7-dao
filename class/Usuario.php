1313d52478c55bcf09b9b8ed453b0a70ef6d2700
Paulo HAS <phas2010@gmail.com>
2020-05-29 20:25:03 -0300

    Criação de metodos de listagem

--- class/Usuario.php
+++ class/Usuario.php
@@ -8,35 +8,35 @@ class Usuario{
 	private $dtcadastro;
 
 	public function getIdusario() {
-	    return $this->idusario;
+		return $this->idusario;
 	}
-	 
+
 	public function setIdusario($idusario) {
-	    $this->idusario = $idusario;
+		$this->idusario = $idusario;
 	}
 
 	public function getNome() {
-	    return $this->nome;
+		return $this->nome;
 	}
-	 
+
 	public function setNome($nome) {
-	    $this->nome = $nome;
+		$this->nome = $nome;
 	}
 	
 	public function getSenha() {
-	    return $this->senha;
+		return $this->senha;
 	}
-	 
+
 	public function setSenha($senha) {
-	    $this->senha = $senha;
+		$this->senha = $senha;
 	}
 
 	public function getDtcadastro() {
-	    return $this->dtcadastro;
+		return $this->dtcadastro;
 	}
-	 
+
 	public function setDtcadastro($dtcadastro) {
-	    $this->dtcadastro = $dtcadastro;
+		$this->dtcadastro = $dtcadastro;
 	}
 
 	public function loadById($id){
@@ -55,6 +55,47 @@ class Usuario{
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 	}
+//listar todos usuarios ordenados pelo campo nome
+	public static function getList(){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuario ORDER BY nome");
+	}
+
+	//buscar usuario por um nome expecifico
+
+	public static function search($nome){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuario WHERE nome LIKE :SEARCH ", array(
+			':SEARCH'=>"%".$nome."%"
+
+		));
+	}
+
+	//buscar usuarios autenticados no banco
+	public function login($nome, $senha){
+
+		$sql = new Sql();
+
+		$results = $sql-> select("SELECT * FROM usuario WHERE nome = :NOME AND senha = :SENHA", array(
+			":NOME"=>$nome,
+			":SENHA"=>$senha
+		));
+
+		if (count($results)>0) {
+			$row = $results[0];
+
+			$this->setIdusario($row['idusuario']);
+			$this->setNome($row['nome']);
+			$this->setSenha($row['senha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}else{
+			throw new Exception("Login ou senha inválidos");
+			
+		}
+	}
 
 	public function __toString(){
 		return json_encode(array(
@@ -67,4 +108,4 @@ class Usuario{
 }
 
 
- ?>
+?>
--- index.php
+++ index.php
@@ -8,11 +8,26 @@ $usuarios = $sql->select("SELECT * FROM usuario");
 echo json_encode($usuarios);
 
 */
-
+//lista um unico usuario
+/*
 $user = new Usuario();
 
 $user->loadById(5);
 
 echo $user;
+*/
+//lista de varios usuario
+/*
+$lista = Usuario::search("ia");
 
+echo json_encode($lista)
+*/
+
+//carrega um usuario usando o login e senha
+
+$usuario = new Usuario();
+
+$usuario ->login("paulo","12345");
+
+echo $usuario;
  ?>
